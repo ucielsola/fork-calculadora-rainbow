181cb85e22209e86b23f00c41df4919047eeb765
carpicoder <webcarpi@gmail.com>
2024-07-02 23:34:31 +0200

    corregí aclaraciones, también funciones en js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,31 @@
-//Math.ceil(Math.log(2000 / 570) / Math.log(1.01414))
+//Math.ceil(Math.log(quieroTener / tengo) / Math.log(1.01414))
 
 const cuantoDemoroForm = document.querySelector("#cuanto-demoro");
-const cuantoDemoroActual = document.querySelector("#cuanto-demoro #actual");
-const cuantoDemoroLlegar = document.querySelector("#cuanto-demoro #llegar");
-const cuantoDemoroDias = document.querySelector("#dias-totales");
-const fechaFinal = document.querySelector("#fecha-final");
+const cuantoDemoroTengo = document.querySelector("#cuanto-demoro-tengo");
+const cuantoDemoroQuieroTener = document.querySelector("#cuanto-demoro-quiero-tener");
+const cuantoDemoroDias = document.querySelector("#cuanto-demoro-dias");
+const cuantoDemoroFecha = document.querySelector("#cuanto-demoro-fecha");
 
 cuantoDemoroForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    if (isNaN(cuantoDemoroLlegar.value.trim()) || isNaN(cuantoDemoroActual.value.trim()) || cuantoDemoroActual.value.trim() === "" || cuantoDemoroLlegar.value.trim() === "") {
-        alert("Debés ingresar números en ambos campos");
-    } else if ((Math.ceil(Math.log(cuantoDemoroLlegar.value / cuantoDemoroActual.value) / Math.log(1.01414))) < 0) {
-        alert("El monto actual debe ser menor al monto deseado.")
+
+    if (
+        isNaN(cuantoDemoroQuieroTener.value.trim()) ||
+        isNaN(cuantoDemoroTengo.value.trim())       ||
+        cuantoDemoroTengo.value.trim() === ""       ||
+        cuantoDemoroQuieroTener.value.trim() === ""
+    ) {
+        alert("Debés ingresar números en ambos campos.");
+    } else if (Number(cuantoDemoroQuieroTener.value) <= Number(cuantoDemoroTengo.value)) {
+        alert("El monto actual debe ser menor al monto deseado.");
+    } else if (cuantoDemoroQuieroTener.value < 0 || cuantoDemoroTengo.value < 0) {
+        alert("Los números no pueden ser negativos.");
     } else {
-        const calculo = Math.ceil(Math.log(cuantoDemoroLlegar.value / cuantoDemoroActual.value) / Math.log(1.01414));
-        cuantoDemoroDias.innerText = calculo;
+        const resultado = Math.ceil(Math.log(cuantoDemoroQuieroTener.value / cuantoDemoroTengo.value) / Math.log(1.01414));
+        cuantoDemoroDias.innerText = resultado;
+        cuantoDemoroFecha.innerText = new Date(Date.now() + resultado * 86400000).toLocaleDateString('es-ES');
         cuantoDemoroDias.classList.add("verde");
-        const fecha = new Date(new Date().setDate(new Date().getDate() + calculo)).toLocaleDateString('es-ES');
-        fechaFinal.innerText = fecha;
-        fechaFinal.classList.add("verde");
+        cuantoDemoroFecha.classList.add("verde");
     }
 })
 
@@ -26,23 +33,32 @@ cuantoDemoroForm.addEventListener("submit", (e) => {
 //tengo * Math.pow(1.01414, dias)
 
 const cuantoEnDiasForm = document.querySelector("#cuanto-en-dias");
-const cuantoEnDiasActual = document.querySelector("#actual-dias");
-const cuantoEnDiasLlegar = document.querySelector("#dias");
-const cuantoEnDiasUSDT = document.querySelector("#usdt-en-dias");
-const cuantoEnDiasSpanDias = document.querySelector("#usdt-cuantos-dias");
-const fechaFinalDias = document.querySelector("#fecha-final-dias");
+const cuantoEnDiasTengo = document.querySelector("#cuanto-en-dias-tengo");
+const cuantoEnDiasDiasAOperar = document.querySelector("#cuanto-en-dias-dias-a-operar");
+const cuantoEnDiasDias = document.querySelector("#cuanto-en-dias-dias");
+const cuantoEnDiasUSDT = document.querySelector("#cuanto-en-dias-usdt");
+const cuantoEnDiasFecha = document.querySelector("#cuanto-en-dias-fecha");
 
 cuantoEnDiasForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    if (isNaN(cuantoEnDiasActual.value.trim()) || isNaN(cuantoEnDiasLlegar.value.trim()) || cuantoEnDiasActual.value.trim() === "" || cuantoEnDiasLlegar.value.trim() === "") {
-        alert("Debés ingresar números en ambos campos");
-    } else {
-        cuantoEnDiasUSDT.innerText = (Math.floor(cuantoEnDiasActual.value * Math.pow(1.01414, cuantoEnDiasLlegar.value)));
-        cuantoEnDiasSpanDias.innerText = cuantoEnDiasLlegar.value;
+
+    if (
+        isNaN(cuantoEnDiasTengo.value.trim()) ||
+        isNaN(cuantoEnDiasDiasAOperar.value.trim()) ||
+        cuantoEnDiasTengo.value.trim() === "" ||
+        cuantoEnDiasDiasAOperar.value.trim() === ""
+    ) {
+        alert("Debés ingresar números en ambos campos.");
+    } else if (cuantoEnDiasTengo.value.trim() < 0 || Number(cuantoEnDiasDiasAOperar.value.trim()) < 0) {
+        alert("Los números no pueden ser negativos.");
+    }else {
+        cuantoEnDiasUSDT.innerText = Math.floor(cuantoEnDiasTengo.value * Math.pow(1.01414, cuantoEnDiasDiasAOperar.value));
         cuantoEnDiasUSDT.classList.add("verde");
-        cuantoEnDiasSpanDias.classList.add("verde");
-        const fecha = new Date(new Date().setDate(new Date().getDate() + Number(cuantoEnDiasLlegar.value))).toLocaleDateString('es-ES');
-        fechaFinalDias.innerText = fecha;
-        fechaFinalDias.classList.add("verde");
+
+        cuantoEnDiasDias.innerText = cuantoEnDiasDiasAOperar.value;
+        cuantoEnDiasDias.classList.add("verde");
+
+        cuantoEnDiasFecha.innerText = new Date(Date.now() + Number(cuantoEnDiasDiasAOperar.value) * 86400000).toLocaleDateString('es-ES');
+        cuantoEnDiasFecha.classList.add("verde");
     }
 })
