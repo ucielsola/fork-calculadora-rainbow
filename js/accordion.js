27e1cc67ec0ca6131447fbaae956463d8e308e68
carpicoder <webcarpi@gmail.com>
2024-09-16 02:52:46 +0200

    add accordion chevrons

--- js/accordion.js
+++ js/accordion.js
@@ -3,20 +3,16 @@ var i;
 
 acc.forEach((acc, index) => {
     var panel = acc.nextElementSibling;
+    var chevron = acc.querySelector(".chevron");
     let initialMaxHeight = panel.scrollHeight + "px";
-    panel.style.maxHeight = initialMaxHeight;
-    setTimeout(() => {
-        setTimeout(() => {
-            panel.style.maxHeight = "0px";
-        }, index * 100);
-    }, 500);
     acc.addEventListener("click", function() {
-        console.log(initialMaxHeight, panel.style.maxHeight);
         acc.classList.toggle("active");
         if (panel.style.maxHeight === initialMaxHeight) {
             panel.style.maxHeight = "0px";
+            chevron.style.transform = "rotate(0)";
         } else {
             panel.style.maxHeight = initialMaxHeight;
+            chevron.style.transform = "rotate(180deg)";
         }
     });
 });
